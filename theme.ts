1c660750a1ff9a92b416cd66e2544e7d2f7d39c2
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-06-16 22:26:38 +0300

    Added the Roboto font to the theme as a default font

---
+++ theme.ts
@@ -0,0 +1,8 @@
+import { createTheme } from '@mui/material/styles';
+const theme = createTheme({
+  typography: {
+    fontFamily: ['Roboto', 'sans-serif'].join(','),
+  },
+});
+
+export default theme;
