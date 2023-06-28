e677b1a1b2b7371d8fbb616fccabf22e7390e302
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-06-29 01:55:46 +0300

    Provided the neccessery props for the product Cards

--- components/ProductsShop.tsx
+++ components/ProductsShop.tsx
@@ -7,16 +7,41 @@ const Store: React.FC = () => {
     <>
       <Grid pt={{ sm: 5 }} spacing={2} container justifyContent="center">
         <Grid item>
-          <ProductCard />
+          <ProductCard
+            id="1"
+            name="Aramis"
+            image={{ src: '/product-images/aramis.jpg', alt: 'Aramis perfume' }}
+            price={100}
+            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto alias, eaque, aliquid, voluptate nesciunt unde autem est corrupti odit expedita cum delectus at temporibus? Nemo illum excepturi fugit "
+          />
         </Grid>
         <Grid item>
-          <ProductCard />
+          <ProductCard
+            id="2"
+            name="Aramis"
+            price={150}
+            image={{ src: '/product-images/aramis.jpg', alt: 'Aramis perfume' }}
+            text="Lorem, ipsum do, eaque, aliquid, voluptate nesciunt unde autem est corrupti odit expedita cum delectus at temporibus? Nemo illum excepturi fugit "
+          />
         </Grid>
         <Grid item>
-          <ProductCard />
+          <ProductCard
+            id="3"
+            name="Aramis"
+            price={120}
+            image={{ src: '/product-images/aramis.jpg', alt: 'Aramis perfume' }}
+            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto alias, eaque, aliquid, voluptate nesciunt unde autem est corrupti odit expedita cum delectus at temporibus? Nemo illum excepturi fugit "
+          />
         </Grid>
         <Grid item>
-          <ProductCard />
+          <ProductCard
+            id="4"
+            name="Aramis"
+            price={310}
+            image={{ src: '/product-images/aramis.jpg', alt: 'Aramis perfume' }}
+            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto alias, eaque, aliquid, voluptate nesciunt unde autem est corrupti odit expedita cum delectus at temporibus? Nemo illum excepturi fugit "
+            soldOut={true}
+          />
         </Grid>
       </Grid>
     </>
