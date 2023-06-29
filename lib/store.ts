faaf19b6deb22a883311820fab5b24ba582eb3ef
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-06-29 16:27:23 +0300

    Added the products reducer

--- lib/store.ts
+++ lib/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cart from './slices/cartSlice';
+import products from './slices/productSlice';
 
 export const store = configureStore({
   reducer: {
     cart,
+    products,
   },
 });
 
