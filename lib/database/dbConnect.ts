a9581eb368bec2b39b5ceff26677d836dcdf6348
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-06-29 16:21:11 +0300

    Added the connect function to the database

---
+++ lib/database/dbConnect.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+async function dbConnect() {
+  if (!MONGODB_URI) {
+    throw new Error(
+      'Please define the MONGODB_URI environment variable inside .env.local'
+    );
+  }
+  if (mongoose.connections[0].readyState) {
+    console.log('connected to database.');
+  } else {
+    console.log(`database isn't connected.`);
+    console.log('connecting to database...');
+    await mongoose.connect(MONGODB_URI);
+    console.log('connected to database.');
+  }
+}
+
+export default dbConnect;
