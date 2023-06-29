7f3344c32ba6aca318ad0fa782b3205231df0fe0
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-06-29 16:26:58 +0300

    Added the useSelector hook to get the products

--- components/ProductsShop.tsx
+++ components/ProductsShop.tsx
@@ -1,51 +1,35 @@
 import React from 'react';
 import { Grid } from '@mui/material';
+import { RootState } from '@/lib/store';
+import { useSelector } from 'react-redux';
+import productType from '@/types/productType';
+
 import ProductCard from './Card';
 
-const Store: React.FC = () => {
+function Store() {
+  const products = useSelector((state: RootState) => state.products.products);
   return (
     <>
       <Grid pt={{ sm: 5 }} spacing={2} container justifyContent="center">
-        <Grid item>
-          <ProductCard
-            id="1"
-            name="Aramis"
-            image={{ src: '/product-images/aramis.jpg', alt: 'Aramis perfume' }}
-            price={100}
-            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto alias, eaque, aliquid, voluptate nesciunt unde autem est corrupti odit expedita cum delectus at temporibus? Nemo illum excepturi fugit "
-          />
-        </Grid>
-        <Grid item>
-          <ProductCard
-            id="2"
-            name="Aramis"
-            price={150}
-            image={{ src: '/product-images/aramis.jpg', alt: 'Aramis perfume' }}
-            text="Lorem, ipsum do, eaque, aliquid, voluptate nesciunt unde autem est corrupti odit expedita cum delectus at temporibus? Nemo illum excepturi fugit "
-          />
-        </Grid>
-        <Grid item>
-          <ProductCard
-            id="3"
-            name="Aramis"
-            price={120}
-            image={{ src: '/product-images/aramis.jpg', alt: 'Aramis perfume' }}
-            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto alias, eaque, aliquid, voluptate nesciunt unde autem est corrupti odit expedita cum delectus at temporibus? Nemo illum excepturi fugit "
-          />
-        </Grid>
-        <Grid item>
-          <ProductCard
-            id="4"
-            name="Aramis"
-            price={310}
-            image={{ src: '/product-images/aramis.jpg', alt: 'Aramis perfume' }}
-            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto alias, eaque, aliquid, voluptate nesciunt unde autem est corrupti odit expedita cum delectus at temporibus? Nemo illum excepturi fugit "
-            soldOut={true}
-          />
-        </Grid>
+        {products.map((product, index) => {
+          return (
+            <Grid item key={index}>
+              <ProductCard
+                id={product.id}
+                name={product.name}
+                price={product.price}
+                image={{
+                  src: product.image,
+                  alt: `${product.name} perfume`,
+                }}
+                text={product.description}
+              />
+            </Grid>
+          );
+        })}
       </Grid>
     </>
   );
-};
+}
 
 export default Store;
