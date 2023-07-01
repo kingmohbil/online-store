43fc73eea315fabe62c3b53530e8301301d4cdff
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-07-02 01:22:55 +0300

    Added the Head layout containg the title for all the pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,15 @@ import { ThemeProvider } from '@mui/material/styles';
 import theme from '@/theme';
 import { store } from '@/lib/store';
 import { Provider } from 'react-redux';
-
+import Head from 'next/head';
+import { websiteName } from '@/constants';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Provider store={store}>
+        <Head>
+          <title>{websiteName}</title>
+        </Head>
         <Component {...pageProps} />
       </Provider>
     </ThemeProvider>
