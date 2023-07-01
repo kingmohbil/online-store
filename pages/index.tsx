c87aef671adb9f19ec96647e9c68a7ca9090432b
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-07-02 01:28:46 +0300

    Removed the title from the component

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,6 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>{websiteName}</title>
         <meta
           name="description"
           content="Home page for a brief description about the store"
