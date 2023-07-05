940510c19e08acaf5bc474258bc8254b1f0b13a6
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-07-05 16:05:52 +0300

    Added all the invoices emails

--- lib/mailSender.ts
+++ lib/mailSender.ts
@@ -1,6 +1,10 @@
 const sdGrid = require('@sendgrid/mail');
-const InvoiceTemplateId = 'd-253a7195927a42eaa57837464332d6cd';
+const CustomerInvoiceTemplateId = 'd-253a7195927a42eaa57837464332d6cd';
+const ManagerTemplateId = 'd-0b5298e405b34baeb66116931a661717';
+const DeliveryTemplateId = 'd-d091704babfe41659f60c3a676fb1457';
+const ProductProviderTemplateId = 'd-48e1e264952f4c0289bddbfbbdca06b0';
 
+// customer invoice details
 interface InvoiceDetails {
   orderId: string;
   customerName: string;
@@ -17,18 +21,51 @@ interface InvoiceDetails {
   total: string;
 }
 
+interface ManagerEmailDetails extends InvoiceDetails {
+  managerEmail: string;
+  customerPhone: string;
+  customerSecondPhone: string;
+  customerLocation: string;
+  paymentMethod: string;
+}
+
+interface DeliveryDetails {
+  orderId: string;
+  customerName: string;
+  customerPhone: string;
+  customerSecondPhone: string;
+  customerLocation: string;
+  customerPayment: string;
+}
+
+interface ProductProviderDetails {
+  orderId: string;
+  orderDetails: {
+    products: [
+      {
+        name: string;
+        quantity: string;
+      }
+    ];
+  };
+}
+// send grid api key
+sdGrid.setApiKey(process.env.SEND_GRID_API_KEY);
+
 export async function sendCustomerInvoice(invoice: InvoiceDetails) {
-  sdGrid.setApiKey(process.env.SEND_GRID_API_KEY);
   const message = {
+    // sender email
     from: { email: process.env.SENDER_EMAIL, name: 'MR PERFUMES' },
     personalizations: [
       {
+        // receiver email
         to: [
           {
             email: invoice.customerEmail,
           },
         ],
-        dynamic_template_data: {
+        // email data
+        dynamic_template_invoice: {
           customerName: invoice.customerName,
           orderId: invoice.orderId,
           orderDetails: invoice.orderDetails,
@@ -36,7 +73,103 @@ export async function sendCustomerInvoice(invoice: InvoiceDetails) {
         },
       },
     ],
-    template_id: InvoiceTemplateId,
+    template_id: CustomerInvoiceTemplateId,
+  };
+  try {
+    return Promise.resolve(sdGrid.send(message));
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
+
+export async function sendManagerInvoice(invoice: ManagerEmailDetails) {
+  const message = {
+    // sender email
+    from: { email: process.env.SENDER_EMAIL, name: 'MR PERFUMES' },
+    personalizations: [
+      {
+        // receiver email
+        to: [
+          {
+            email: process.env.MANAGER_EMAIL,
+          },
+        ],
+        // manager invoice data
+        dynamic_template_invoice: {
+          orderId: invoice.orderId,
+          customerName: invoice.customerName,
+          customerEmail: invoice.customerEmail,
+          customerPhone: invoice.customerPhone,
+          customerSecondPhone: invoice.customerSecondPhone,
+          customerLocation: invoice.customerLocation,
+          orderDetails: invoice.orderDetails,
+          total: invoice.total,
+          paymentMethod: invoice.paymentMethod,
+        },
+      },
+    ],
+    template_id: ManagerTemplateId,
+  };
+  try {
+    return Promise.resolve(sdGrid.send(message));
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
+
+export async function sendDeliveryEmail(invoice: DeliveryDetails) {
+  const message = {
+    // sender email
+    from: { email: process.env.SENDER_EMAIL, name: 'MR PERFUMES' },
+    personalizations: [
+      {
+        // receiver email
+        to: [
+          {
+            email: process.env.DELIVERY_EMAIL,
+          },
+        ],
+        // delivery email date
+        dynamic_template_invoice: {
+          orderId: invoice.orderId,
+          customerName: invoice.customerName,
+          customerPhone: invoice.customerPhone,
+          customerSecondPhone: invoice.customerSecondPhone,
+          customerPayment: invoice.customerPayment,
+        },
+      },
+    ],
+    template_id: DeliveryTemplateId,
+  };
+  try {
+    return Promise.resolve(sdGrid.send(message));
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
+
+export async function sendProductProviderEmail(
+  invoice: ProductProviderDetails
+) {
+  const message = {
+    // sender email
+    from: { email: process.env.SENDER_EMAIL, name: 'MR PERFUMES' },
+    personalizations: [
+      {
+        // receiver email
+        to: [
+          {
+            email: process.env.PRODUCT_PROVIDER_EMAIL,
+          },
+        ],
+        // product provider email data
+        dynamic_template_invoice: {
+          orderId: invoice.orderId,
+          orderDetails: invoice.orderDetails,
+        },
+      },
+    ],
+    template_id: ProductProviderTemplateId,
   };
   try {
     return Promise.resolve(sdGrid.send(message));
