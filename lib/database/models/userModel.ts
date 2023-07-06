d0b75a4638b3f89e923218b4d5aa1d5687c0e917
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-07-06 20:34:13 +0300

    Added user model

---
+++ lib/database/models/userModel.ts
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  first_name: {
+    type: String,
+    minLength: 3,
+    maxLength: 20,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    minLength: 3,
+    maxLength: 20,
+    required: true,
+  },
+  email: {
+    email_address: {
+      type: String,
+      required: true,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  contact_details: {
+    phone_number: {
+      type: String,
+      minLength: 10,
+      maxLength: 10,
+      required: true,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  created_at: {
+    type: Schema.Types.Date,
+    default: () => Date.now(),
+  },
+  orders: [
+    {
+      type: [Schema.Types.ObjectId],
+      red: 'orders',
+    },
+  ],
+});
+
+module.exports = mongoose.models.users || mongoose.model('users', userSchema);
