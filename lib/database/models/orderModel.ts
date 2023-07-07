9b0f6926a4846b8a5b50d2112b51558eeecdc50c
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-07-07 23:56:55 +0300

    Filled the order Schema with the required fields

--- lib/database/models/orderModel.ts
+++ lib/database/models/orderModel.ts
@@ -2,7 +2,59 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-const orderSchema = new Schema({});
+const orderSchema = new Schema({
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone_number: {
+    type: String,
+    minLength: 10,
+    maxLength: 10,
+    required: true,
+  },
+  location_details: {
+    City: {
+      type: String,
+      enum: ['Amman'],
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+  },
+  order_details: {
+    products: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    delivery_fees: {
+      type: Number,
+      required: true,
+    },
+  },
+});
 
 module.exports =
   mongoose.models.orders || mongoose.model('orders', orderSchema);
