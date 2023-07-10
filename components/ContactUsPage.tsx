d2074ef375cd3b085ae8f49f0ee28394b3803525
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-07-10 03:11:22 +0300

    Added contact us component

---
+++ components/ContactUsPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+function ContactUs() {
+  return <Box></Box>;
+}
+export default ContactUs;
---
+++ pages/contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppBar from '@/components/AppBar';
+
+function ContactUsPage() {
+  return (
+    <>
+      <AppBar />
+    </>
+  );
+}
+
+export default ContactUsPage;
