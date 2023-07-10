2e0a87a99ff4f99b28bef7110e8fa9fc39f3dcd5
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-07-11 01:31:20 +0300

    Added the route for requsting access token

---
+++ pages/api/auth/access_token.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { createRouter } from 'next-connect';
+import { generateAccessToken } from '@/lib/helpers/generateTokens';
+import { verifyRefreshToken } from '@/lib/helpers/verifyTokens';
+const jwt = require('jsonwebtoken');
+
+const router = createRouter<NextApiRequest, NextApiResponse>().post(
+  verifyRefreshToken,
+  async (req, res) => {
+    const authHeader = req.headers?.['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    try {
+      const user = await jwt.verify(
+        token,
+        process.env.REFRESH_TOKEN_SECRET_KEY
+      );
+      const accessToken = await generateAccessToken({ userId: user.userId });
+      return res.json({ accessToken, message: 'Refresh token verified' });
+    } catch (error) {
+      return res.status(401).json({ message: "Refresh token isn't valid" });
+    }
+  }
+);
+
+export default router.handler({
+  onNoMatch: async (req, res) => {
+    res.status(405).json({ errorMessage: 'Method Not Allowed' });
+  },
+});
