ae3a6f269c3fc77445b48af5847716e308a62bed
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-07-11 01:10:46 +0300

    Added protection for the route

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,48 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import LoginForm from '@/components/LoginForm';
 import { Paper, Box } from '@mui/material';
 
 function LoginPage() {
-  return (
-    <>
-      <Head>
-        <title>Login</title>
-      </Head>
-      <Box
-        sx={{
-          minHeight: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Paper
+  const router = useRouter();
+  const [loggedIn, setLoggedIn] = useState(false);
+  useEffect(() => {
+    if (localStorage.getItem('refreshToken') != null) setLoggedIn(true);
+    else setLoggedIn(false);
+  }, []);
+  if (loggedIn) {
+    router.push('/');
+  } else
+    return (
+      <>
+        <Head>
+          <title>Login</title>
+        </Head>
+        <Box
           sx={{
-            px: {
-              xs: 2,
-            },
-            pt: {
-              xs: 5,
-            },
-            pb: {
-              xs: 3,
-            },
-            width: {
-              xs: 300,
-              sm: 500,
-            },
+            minHeight: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
           }}
-          elevation={10}
         >
-          <Box>
-            <LoginForm />
-          </Box>
-        </Paper>
-      </Box>
-    </>
-  );
+          <Paper
+            sx={{
+              px: {
+                xs: 2,
+              },
+              pt: {
+                xs: 5,
+              },
+              pb: {
+                xs: 3,
+              },
+              width: {
+                xs: 300,
+                sm: 500,
+              },
+            }}
+            elevation={10}
+          >
+            <Box>
+              <LoginForm />
+            </Box>
+          </Paper>
+        </Box>
+      </>
+    );
 }
 
 export default LoginPage;
