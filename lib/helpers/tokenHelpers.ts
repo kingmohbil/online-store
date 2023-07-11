86b35f51800961586abc64e657d68fd90bc243c8
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-07-11 22:08:34 +0300

    Added token helper functions

---
+++ lib/helpers/tokenHelpers.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+export function clearTokensFromLocalStorage() {
+  localStorage.removeItem('accessToken');
+  localStorage.removeItem('refreshToken');
+}
+
+export async function requestAccessToken(refreshToken: string) {
+  try {
+    const response = await axios.get('/api/auth/access_token', {
+      headers: {
+        Authorization: `Bearer ${refreshToken}`,
+      },
+    });
+    if (response.status === 200) return response.data.accessToken;
+  } catch (error: any) {
+    if (error.response.status === 401) return null;
+  }
+}
+
+export async function logoutTokens(refreshToken: string) {
+  try {
+    const logoutResponse = await axios.get('/api/auth/logout', {
+      headers: {
+        Authorization: `Bearer ${refreshToken}`,
+      },
+    });
+    if (logoutResponse.status === 200) {
+      clearTokensFromLocalStorage();
+      return true;
+    }
+  } catch (error) {
+    return false;
+  }
+}
