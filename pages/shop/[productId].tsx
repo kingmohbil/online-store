24c0dc3184927effc057bd74734bef356b3e720b
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-07-19 23:53:32 +0300

    Added 404 error for not found pages

--- pages/shop/[productId].tsx
+++ pages/shop/[productId].tsx
@@ -51,7 +51,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     console.log(error);
     return {
       props: {
-        JSONProduct: '',
+        notFound: true,
       },
     };
   }
