81941c978244dc68c6416479e5600c331c8e8cdd
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-07-19 03:14:49 +0300

    Added a contact links

---
+++ components/Footer.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import Link from 'next/link';
+import { Box, Typography } from '@mui/material';
+import {
+  mainPageXMargins,
+  whatsAppNumber,
+  instagramProfileName,
+  supportEmail,
+} from '../constants';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+
+function Footer() {
+  return (
+    <>
+      <Box
+        sx={{
+          ...mainPageXMargins,
+
+          pb: 1,
+        }}
+      >
+        <Box display="flex" justifyContent="right">
+          <Box>
+            <Typography sx={{ pb: 1, color: 'var(--gold-color)' }}>
+              Contact us
+            </Typography>
+            <Box sx={{ display: 'flex', gap: 4 }}>
+              <Link
+                href={`https://api.whatsapp.com/send?phone=${whatsAppNumber}`}
+                target="_blank"
+                aria-label="Link to contact us whatsapp"
+              >
+                <WhatsAppIcon sx={{ color: 'var(--gold-color)' }} />
+              </Link>
+              <Link
+                href={`https://www.instagram.com/${instagramProfileName}/`}
+                target="_blank"
+                aria-label="Link to contact us instagram"
+              >
+                <InstagramIcon sx={{ color: 'var(--gold-color)' }} />
+              </Link>
+              <Link
+                href={`mailto:${supportEmail}/`}
+                target="_blank"
+                aria-label="Link to contact us email"
+              >
+                <EmailOutlinedIcon sx={{ color: 'var(--gold-color)' }} />
+              </Link>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
+}
+
+export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,28 @@
 import Head from 'next/head';
 import AppBar from '@/components/AppBar';
 import HomeComponent from '@/components/HomePage';
+import BackToTop from '@/components/BackToTop';
+import Footer from '@/components/Footer';
+import { Box } from '@mui/material';
 import { websiteName } from '@/constants';
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <meta
-          name="description"
-          content="Home page for a brief description about the store"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <AppBar />
-      <HomeComponent />
+      <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+        <BackToTop />
+        <Head>
+          <meta
+            name="description"
+            content="Home page for a brief description about the store"
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <AppBar />
+        <HomeComponent />
+        <Box sx={{ flexGrow: 1 }}></Box>
+        <Footer />
+      </Box>
     </>
   );
 }
